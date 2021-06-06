3e968bf3660e421ddbcb6b57b209b950f97ef9e6
Luke <munozluke1@gmail.com>
2021-06-06 11:52:45 -0600

    making a branch for the fetch and api

--- assets/script/infopage.js
+++ assets/script/infopage.js
@@ -1,14 +1,21 @@
-fetch("https://trailapi-trailapi.p.rapidapi.com/?q-activities_activity_type_name_eq=hiking&radius=25&q-state_cont=California&q-country_cont=Australia&q-city_cont=Denver&limit=10", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-key": "c7775c3c57mshc8f454db83cbe91p1279fejsn022ef1c7154c",
-		"x-rapidapi-host": "trailapi-trailapi.p.rapidapi.com"
-	}
-})
-.then(response => {
-	console.log(response);
-})
-.catch(err => {
-	console.error(err);
-});
+var apiKey = "c7775c3c57mshc8f454db83cbe91p1279fejsn022ef1c7154c";
+var apiHost = "trailapi-trailapi.p.rapidapi.com";
 
+function myFuntion() {
+    fetch("https://trailapi-trailapi.p.rapidapi.com/?q-activities_activity_type_name_eq=hiking&radius=25&q-state_cont=California&q-country_cont=Australia&q-city_cont=Denver&limit=10" +
+            apiKey +
+            apiHost
+
+        )
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(response) {
+            console.log(response.data)
+        })
+
+
+
+
+}
+myFuntion()
