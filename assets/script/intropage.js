519c72f4185c5215e2bf42df7e59ebea00a6920c
Luke <munozluke1@gmail.com>
2021-06-06 17:26:28 -0600

    api

--- assets/script/intropage.js
+++ assets/script/intropage.js
@@ -1,19 +1,21 @@
-var api = "http://api.amp.active.com/camping/campsites?"
-var apiKey = "tu627n99ahbtf6uyggdudbt9"
-var apiQuery = "rvSites"
+var hikeLengthEl = document.getElementById('hikeLength')
+var hikeDifficultyEl = document.getElementById('hikeDifficulty')
+var hikeSubmitEl = document.getElementById('hikeSubmit')
 
 function myFunction() {
-    fetch(
-            api +
-            apiQuery +
-            apiKey
-        )
-        .then(function(response) {
-            return response.json();
+    fetch("https://trailapi-trailapi.p.rapidapi.com/?q-activities_activity_type_name_eq=hiking&radius=25&q-state_cont=California&q-country_cont=Australia&q-city_cont=Denver&lon=-105.2&limit=25&lat=34.1", {
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-key": "c7775c3c57mshc8f454db83cbe91p1279fejsn022ef1c7154c",
+                "x-rapidapi-host": "trailapi-trailapi.p.rapidapi.com"
+            }
         })
-        .then(function(response) {
+        .then(response => {
             console.log(response);
         })
-
+        .catch(err => {
+            console.error(err);
+        });
 }
-myFunction();
+
+hikeLengthEl.addEventListener('click', lengthInput)
