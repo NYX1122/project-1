a3a6411e3dfe613fdaa86b0310793abbdda74d5a
BoDee <nyx0hemir@gmail.com>
2021-06-07 21:10:48 -0600

    finished styling of parameter entries

---
+++ assets/script/resultspage.js
@@ -0,0 +1,15 @@
+fetch("https://trailapi-trailapi.p.rapidapi.com/trails/explore/?lon=-112.032997&lat=40.514114", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-key": "c6c5ba3134msh910d133557b3135p17ea21jsnc3a6398170be",
+		"x-rapidapi-host": "trailapi-trailapi.p.rapidapi.com"
+	}
+})
+.then(function(response) { 
+	response.json().then(function(data) {
+		console.log(data);
+	});
+})
+.catch(function(error) {
+	alert("Unable to connect to servers");
+});
