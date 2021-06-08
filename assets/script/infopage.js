3034337506a2b2a47e0edd66cd3cd5102d61d2d5
BoDee <nyx0hemir@gmail.com>
2021-06-07 19:51:36 -0600

    design edit

--- assets/script/infopage.js
+++ assets/script/infopage.js
@@ -1,14 +1,16 @@
-fetch("https://trailapi-trailapi.p.rapidapi.com/?q-activities_activity_type_name_eq=hiking&radius=25&q-state_cont=California&q-country_cont=Australia&q-city_cont=Denver&limit=10", {
+fetch("https://trailapi-trailapi.p.rapidapi.com/trails/explore/?lon=-112.032997&lat=40.514114", {
 	"method": "GET",
 	"headers": {
-		"x-rapidapi-key": "c7775c3c57mshc8f454db83cbe91p1279fejsn022ef1c7154c",
+		"x-rapidapi-key": "c6c5ba3134msh910d133557b3135p17ea21jsnc3a6398170be",
 		"x-rapidapi-host": "trailapi-trailapi.p.rapidapi.com"
 	}
 })
-.then(response => {
-	console.log(response);
+.then(function(response) { 
+	response.json().then(function(data) {
+		console.log(data);
+	});
 })
-.catch(err => {
-	console.error(err);
+.catch(function(error) {
+	alert("Unable to connect to servers");
 });
 
