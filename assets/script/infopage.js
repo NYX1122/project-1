4638f8767023fdaa9c51b8941ac747de54dec2a9
BoDee <nyx0hemir@gmail.com>
2021-06-10 04:13:11 -0600

    finished the second page

--- assets/script/infopage.js
+++ assets/script/infopage.js
@@ -1,26 +1,41 @@
 var inputTextEl = $("#input-text");
 var questionEl = $("#more-info-page-title");
 var formWrapperEl = $("#form-holder");
+var selectInput = $("<select>");
 var questionsArr = ["What is your name?", "What is your address / city?", "What is your maximum travel distance in miles?", "What is your experience level?", "What day would you like to go biking?"];
+var locationData = ""
 var trackerObj = {name: "", address: "", range: "", expertise: "", date: ""};
 
 var submitHandler = function(string) {
+	console.log(string);
 	if (inputTextEl.attr("placeholder") === "name") {
 		trackerObj.name = string;
 	}
 	if (inputTextEl.attr("placeholder") === "address") {
 		trackerObj.address = string;
+		if(string === ""){
+			alert("Please enter a city name.");
+			inputTextEl.attr("placeholder", "name");
+		}
+		else {
+			addressSubmitHandler(string);
+		}
 	}
 	if (inputTextEl.attr("placeholder") === "range") {
 		trackerObj.range = string;
+		string = parseInt(string);
+		if(string === "" || typeof string === "string"){
+			alert("Please enter a number");
+			inputTextEl.attr("placeholder", "address");
+		}
 	}
-	if (inputTextEl.attr("name") === "expertise") {
+	if (selectInput.attr("name") === "expertise") {
 		trackerObj.expertise = string;
 	}
 	if (inputTextEl.attr("placeholder") === "date") {
 		trackerObj.date = string;
 	}
-}
+};
 
 var reset = function() {
 	inputTextEl.val("");
@@ -33,23 +48,59 @@ var reset = function() {
 		inputTextEl.attr("placeholder", "range");
 	}
 	else if (inputTextEl.attr("placeholder") === "range") {
-		questionEl.text(questionsArr[3])
+		questionEl.text(questionsArr[3]);
+		inputTextEl.attr("placeholder", "");
 		inputTextEl.detach();
-		var selectInput = $("<select>");
-		selectInput.html("name='expertise' class='col-12 mb-3 fs-3 form-select'><option selected>Beginner</option><option>Intermediate</option><option>Advanced</option>");
-		formWrapperEl.append(selectInput);
+		selectInput.attr("name", "expertise");
+		selectInput.addClass("col-12 form-select form-select-lg mb-3");
+		selectInput.html("<option selected>Beginner</option><option>Intermediate</option><option>Advanced</option>");
+		$("#next-question").before(selectInput);
 	}
 	else if (selectInput.attr("name") === "expertise") {
-		questionEl.text(questionsArr[4])
+		questionEl.text(questionsArr[4]);
+		selectInput.attr("name", "");
 		selectInput.detach();
-		formWrapperEl.append(inputTextEl);
+		$("#next-question").before(inputTextEl);
+		$("#next-question").text("Get Results");
+		inputTextEl.attr("placeholder", "date");
+		inputTextEl.datepicker({
+			minDate: 1,
+			onClose: function() {
+			}
+		});
 	}
 	else if (inputTextEl.attr("placeholder") === "date") {
+		location.replace("./ResultsPage.html");
 	}
-}
+};
+
+var addressSubmitHandler = function(address) {
+    var geocodeApi = "http://api.positionstack.com/v1/forward?access_key=6a0a7bfe7991fb3b771c2cfee43f426b&query=";
+    var fetchGeocodeUrl = geocodeApi + address;
+	fetch(fetchGeocodeUrl).then(function(response) {
+		if (response.ok) {
+			response.json().then(function(data) {
+				locationData = data;
+			});
+		}
+		else {
+			alert("City not found. Reloading.");
+			location.reload();
+		}
+	})
+	.catch(function(error) {
+		alert("Unable to connect to servers. Reloading.");
+		location.reload();
+	});
+};
 
 $("#next-question").on("click", function() {
-	submitHandler(inputTextEl.val());
+	if(selectInput.attr("name") === "expertise") {
+		submitHandler(selectInput.val());
+	}
+	else {
+		submitHandler(inputTextEl.val());
+	}
 	reset();
 	console.log(trackerObj);
 });
